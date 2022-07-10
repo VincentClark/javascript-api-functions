6dcd1fe2cda08fdbb6c0830d1926f4eae202e177
Vincent Clark <vincentclark@gmail.com>
2022-07-10 12:47:04 -0700

    ready to put this all together

--- service_sid.js
+++ service_sid.js
@@ -4,8 +4,11 @@ const axios = require('axios');
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const requestUrl = `https://${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}@serverless.twilio.com/v1/Services`;
-//https://serverless.twilio.com/v1/Services/ZSd40d69ad093d7d4cce9468ece50e6210/Functions
+//https://serverless.twilio.com/v1/Services/ZS186e4c332336cd165b8edede4c37cd5a/Builds
 
+const buildUrl = `https://${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}@serverless.twilio.com/v1/Services/ZS186e4c332336cd165b8edede4c37cd5a/Builds`;
+//https://serverless.twilio.com/v1/Services/ZSd40d69ad093d7d4cce9468ece50e6210/Functions
+//SERVICE BUILD > https://serverless.twilio.com/v1/Services/ZScf622767d7b278463c37bcbab181c4b2/Builds/ZB3921e0ef51fd90689dd5a8e77c8445b1
 const options = {
     method: 'GET',
     headers: {
@@ -74,10 +77,26 @@ const serviceApi = async () => {
         );
 }
 
+const buildSids = async () => {
+    const axioResponse = await axios.get(buildUrl)
+        .then(response => {
+            console.log(response.data);
+            // return service.sid;
+            //console.log(service);
+            console.log("buildUrl", buildUrl);
 
+        })
+        .catch(error => {
+            console.log("error");
+            console.log("buildUrl", buildUrl);
+        }
+        );
+}
+
+//buildSids();
 //serviceApi();
-//functionResonseGenerator("ZSd40d69ad093d7d4cce9468ece50e6210");
-assetResonseGenerator("ZScf622767d7b278463c37bcbab181c4b2");
+functionResponseGenerator("ZS20c9d69506440933bc9cf3fe01b302b0");
+//assetResonseGenerator("ZScf622767d7b278463c37bcbab181c4b2");
 
 /*
 Service Friendly Name: voicemail
